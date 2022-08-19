8b0a64f4e0c3d0642e291dbf070c59ad05813d9d
Ben Dechrai <ben@dechrai.com>
2022-08-19 12:57:19 +1000

    WIP

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h2>About</h2>
+      <p>
+        Hi! My name is Ben, and I like to travel. I do a fair amount for work
+        and personally, and I love to maximise my status earn for as few dollars
+        as possible.
+      </p>
+      <p>
+        This means I'm usually on the lookout for interesting routes, and tools
+        like Qantas's booking system, Expert Flyer, and even Google Flights help
+        out. (Google Flights is actually a great way to find cheap multi-stop
+        trips.)
+      </p>
+      <p>
+        Please use this site to your heart's content, and if you find any
+        issues, <a href="https://twitter.com/bendechrai">ping me on Twitter</a>.
+        My DMs are open, and I love to chat with fellow travellers!
+      </p>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,397 @@
+/**
+ *{
+    "points": [
+        {
+            "zero_earn": "N",
+            "fare_type": "N",
+            "distance": 1643,
+            "fare_class": "Discount Economy",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Discount Economy",
+            "base": 1000,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Discount Economy",
+            "status_credits": 20,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1000,
+            "mpg": 800,
+            "base_plus_cabin_bonus": 2000.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 2000.0,
+            "cabin_bonus": 1000.0,
+            "status_tier": "Platinum",
+            "debug_route_info": {
+                "sector": "TransTasman Routes",
+                "route_to_segment_start": "East Coast Australia",
+                "index": "QFQFMELAKL",
+                "segment_end": "New Zealand or Papua New Guinea",
+                "loyalty_non_earn_categories": [
+                    "Classic Flight Reward Business",
+                    "Classic Flight Reward Premium Economy",
+                    "First",
+                    "Classic Flight Reward Economy"
+                ],
+                "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+                "origin": "MEL",
+                "segment_start": "Between ADL,BNE,OOL,MEL,SYD and",
+                "origin_region": "East Coast Australia",
+                "type": "route",
+                "destination": "AKL",
+                "origin_country": "AU",
+                "loyalty_earn_categories": [
+                    "Premium Economy",
+                    "Discount Business",
+                    "Business",
+                    "Flexible Business",
+                    "Economy",
+                    "Discount Premium Economy",
+                    "First",
+                    "Flexible Premium Economy",
+                    "Flexible Economy",
+                    "Discount Economy"
+                ],
+                "distance": 1643,
+                "only_set_sector": "N",
+                "qf_operated": true,
+                "owner_airline": "QF",
+                "destination_region": "New Zealand",
+                "destination_country": "NZ",
+                "zone_rule": "RegionCountry-StartEnd",
+                "carrier": "QF",
+                "route_category": "TransTasman Routes",
+                "route_to_segment_end": "NZ",
+                "distance_base": 1643.0423334720474
+            }
+        },
+        {
+            "upgrades": [
+                [
+                    "business",
+                    21800
+                ]
+            ],
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Economy",
+            "origin": "MEL",
+            "type": "detail",
+            "destination": "AKL",
+            "status_credits": 25,
+            "zero_earn": "N",
+            "distance": 1643,
+            "fare_type": "G",
+            "fare_class": "Economy",
+            "base": 1375,
+            "route_category": "TransTasman Routes",
+            "loyalty_earn_category": "Economy",
+            "min_of_base_and_ecoflex": 1375,
+            "mpg": 800,
+            "base_plus_cabin_bonus": 2750.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 2750.0,
+            "cabin_bonus": 1375.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "upgrades": [
+                [
+                    "business",
+                    11900
+                ]
+            ],
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Flexible Economy",
+            "origin": "MEL",
+            "type": "detail",
+            "destination": "AKL",
+            "status_credits": 40,
+            "zero_earn": "N",
+            "distance": 1643,
+            "fare_type": "Y",
+            "fare_class": "Flexible Economy",
+            "base": 1750,
+            "route_category": "TransTasman Routes",
+            "loyalty_earn_category": "Flexible Economy",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1200,
+            "base_plus_cabin_bonus": 3500.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 3500.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "T",
+            "distance": 1643,
+            "fare_class": "Discount Premium Economy",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Discount Premium Economy",
+            "base": 1750,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Discount Premium Economy",
+            "status_credits": 40,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1200,
+            "base_plus_cabin_bonus": 3500.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 3500.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "R",
+            "distance": 1643,
+            "fare_class": "Premium Economy",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Premium Economy",
+            "base": 2125,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Premium Economy",
+            "status_credits": 45,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1200,
+            "base_plus_cabin_bonus": 3875.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 3875.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "W",
+            "distance": 1643,
+            "fare_class": "Flexible Premium Economy",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Flexible Premium Economy",
+            "base": 2300,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Flexible Premium Economy",
+            "status_credits": 50,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1200,
+            "base_plus_cabin_bonus": 4050.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 4050.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "I",
+            "distance": 1643,
+            "fare_class": "Discount Business",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Discount Business",
+            "base": 2500,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Discount Business",
+            "status_credits": 80,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1400,
+            "base_plus_cabin_bonus": 4250.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 4250.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "D",
+            "distance": 1643,
+            "fare_class": "Business",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Business",
+            "base": 2700,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Business",
+            "status_credits": 85,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1400,
+            "base_plus_cabin_bonus": 4450.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 4450.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "J",
+            "distance": 1643,
+            "fare_class": "Flexible Business",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "Flexible Business",
+            "base": 2875,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "Flexible Business",
+            "status_credits": 90,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1400,
+            "base_plus_cabin_bonus": 4625.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 4625.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        },
+        {
+            "zero_earn": "N",
+            "fare_type": "F",
+            "distance": 1643,
+            "fare_class": "First",
+            "profile_zone_description": "ADL,BNE,OOL,MEL,SYD-NZ & PNG",
+            "fare_type_name": "First",
+            "base": 3250,
+            "origin": "MEL",
+            "route_category": "TransTasman Routes",
+            "type": "detail",
+            "loyalty_earn_category": "First",
+            "status_credits": 120,
+            "destination": "AKL",
+            "min_of_base_and_ecoflex": 1750,
+            "mpg": 1400,
+            "base_plus_cabin_bonus": 5000.0,
+            "eco_flex": 1750,
+            "status_tier_cabin_bonus": 1.0,
+            "earn": 5000.0,
+            "cabin_bonus": 1750.0,
+            "status_tier": "Platinum"
+        }
+    ],
+    "loyalty_earn_categories": [
+        {
+            "code": "discounteconomy",
+            "name": "Discount Economy"
+        },
+        {
+            "code": "economy",
+            "name": "Economy"
+        },
+        {
+            "code": "flexibleeconomy",
+            "name": "Flexible Economy"
+        },
+        {
+            "code": "discountpremiumeconomy",
+            "name": "Discount Premium Economy"
+        },
+        {
+            "code": "premiumeconomy",
+            "name": "Premium Economy"
+        },
+        {
+            "code": "flexiblepremiumeconomy",
+            "name": "Flexible Premium Economy"
+        },
+        {
+            "code": "discountbusiness",
+            "name": "Discount Business"
+        },
+        {
+            "code": "business",
+            "name": "Business"
+        },
+        {
+            "code": "flexiblebusiness",
+            "name": "Flexible Business"
+        },
+        {
+            "code": "first",
+            "name": "First"
+        }
+    ]
+}
+ *
+ * */
+import React, { useState } from "react";
+
+const Home = () => {
+  const [results, setResults] = useState("");
+  const [statusLevel, setStatusLevel] = useState("Bronze");
+  const [itinerary, setItinerary] = useState(
+    `
+    MEL SYD (Y)
+    SYD AKL
+    AKL MEL (J)
+    This line won't do anything
+    This will catch the LHR to JFK flight in (D)
+  `
+  );
+
+  const onChangeStatusLevel = (ev) => {
+    setStatusLevel(ev.target.value);
+  };
+
+  const onChangeItinerary = (ev) => {
+    setItinerary(ev.target.value);
+  };
+
+  const calculateItinerary = (ev) => {
+    setResults(`Parsing ${itinerary}`);
+  };
+
+  return (
+    <>
+      <h2>Qantas Itinerary Tool</h2>
+      <p>
+        This site launched on March 29th, and it under continuous(ish)
+        improvement. If something doesn't work, please{" "}
+        <a href="https://twitter.com/bendechrai">ping me on Twitter</a>. My DMs
+        are open, and I love to chat with fellow travellers!
+      </p>
+      <p>
+        Paste your itinerary in the following field. This script will look for
+        two three-character airport codes per line, and use those as a departure
+        and arrive port. If you include a booking class in brackets, the script
+        will take that into account, else you'll get the calculation for the
+        cheapest fare for that segment.
+      </p>
+      <p>
+        To get the right points calculation, please pick your current status
+        level:
+      </p>
+      <select onChange={onChangeStatusLevel} value={statusLevel}>
+        <option value="Bronze">Bronze</option>
+        <option value="Silver">Silver</option>
+        <option value="Gold">Gold</option>
+        <option value="Platinum">Platinum</option>
+        <option value="Platinum One">Platinum One</option>
+      </select>
+      <textarea onChange={onChangeItinerary} value={itinerary} />
+      <button onClick={calculateItinerary}>Calculate</button>
+      <div>
+        <pre>{results}</pre>
+      </div>
+    </>
+  );
+};
+
+export default Home;
